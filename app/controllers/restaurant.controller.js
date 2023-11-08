3c04e93660933c06ad68b3cc409eadcff7a67666
euharajv <euharajv@uregina.ca>
2023-11-07 21:38:25 -0600

    Resturant Controller

---
+++ app/controllers/restaurant.controller.js
@@ -0,0 +1,68 @@
+let bcrypt = require("bcryptjs");
+const ModelFactory = require("../ModelFactory");
+const userModel = ModelFactory.createModel('User');
+const restaurantModel = ModelFactory.createModel('Restaurant');
+
+exports.RestSignup = async (req, res) => {
+    try {
+        const {
+            name,
+            description,
+            phone,
+            email,
+            number_of_table,
+            address,
+            schedules,
+            provide_reservation,
+            table_capacity,
+            categories,
+            image_url
+        } = req.body;
+
+        // Create a new User document for the admin
+        const user = await userModel.create({
+            username: req.body?.username?.toLowerCase(),
+            fullName: req.body?.fullName,
+            email: req.body?.email,
+            password: bcrypt.hashSync(req.body?.password, 8),
+            roles: ['admin'],
+        });
+
+        // Create a new Restaurant document
+        const restaurant = await restaurantModel.create({
+            name,
+            description,
+            phone,
+            email,
+            number_of_table,
+            address,
+            schedules,
+            provide_reservation,
+            table_capacity,
+            categories,
+            image_url,
+            adminId: user._id
+        });
+
+        res.status(201).send({data: restaurant, message: 'Restaurant registered successfully!'});
+    } catch (err) {
+        res.status(500).send({message: err.message});
+    }
+};
+
+
+exports.getAllRestaurants = async (req, res) => {
+    try {
+        // Use the findById method from the restaurantModel to retrieve the restaurant by ID
+        const restaurant = await restaurantModel.find();
+
+        if (!restaurant) {
+            return res.status(404).send({message: 'Restaurant not found.'});
+        }
+
+        res.status(200).send(restaurant);
+    } catch (err) {
+        res.status(500).send({message: err.message});
+    }
+};
+
